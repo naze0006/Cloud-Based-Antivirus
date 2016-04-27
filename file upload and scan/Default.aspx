4197f459f23fc18450894e8c32ae061fe66661b0
Shirin Nazemoroaya <naze0006@algonquinlive.com>
2016-04-26 21:22:13 -0400

    files

---
+++ file upload and scan/Default.aspx
@@ -0,0 +1,73 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .style1
+        {
+            font-family: "Times New Roman", Times, serif;
+            color: #0000FF;
+            font-weight: bold;
+        }
+        .style2
+        {
+            font-size: medium;
+            color: #FF0000;
+        }
+        .style3
+        {
+            font-size: medium;
+            color: #FF0000;
+            font-weight: bold;
+        }
+        .style4
+        {
+            font-size: large;
+        }
+        .style5
+        {
+            font-family: "Times New Roman", Times, serif;
+            color: #FF0000;
+            font-weight: bold;
+        }
+    </style>
+</head>
+<body style="z-index: 1; left: 33px; top: 77px; position: absolute; height: 26px; width: 1271px" 
+    background="upload/12.jpg">
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:FileUpload ID="FileUpload1" runat="server" />
+        <asp:Button ID="Button1" runat="server" onclick="Button1_Click" 
+            Text="Check File" Width="84px" />
+    
+    </div>
+    <p>
+        <asp:Label ID="Label1" runat="server" Text="Label" Visible="False" 
+            ForeColor="#CC0000"></asp:Label>
+    </p>
+    <asp:Panel ID="Panel1" runat="server" Height="311px">
+        <span class="style1">
+        <br />
+        <br />
+        <br />
+        Our Project:<br class="style4" /> </span>
+        <span class="style4"><span class="style5">Antivirus Mcafee As a Service</span></span><span 
+            class="style1"><br />
+        <br />
+        <br />
+        <br />
+        Our Group members:</span><br />
+        <span class="style3">Vahid Moraveji Hashemi</span><b><br class="style2" /> </b>
+        <span class="style3">Ali Falsafi</span><b><br class="style2" /> </b>
+        <span class="style3">Mohammadali Iravani</span><b><br class="style2" /> </b>
+        <span class="style3">Shirin Nazemoroaya</span><b><br class="style2" /> </b>
+        <span class="style3">Ghazaleh Rezayat</span></asp:Panel>
+    </form>
+    <p>
+        &nbsp;</p>
+</body>
+</html>
---
+++ file upload and scan/Default.aspx.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+   
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        if (scanfile())
+        {
+            //Response.Write("file upload succes");
+            Label1.Visible = true;
+            Label1.Text= "No Virus Detected";
+            
+        }
+        else
+        {
+            //Response.Write("file have virus not upload");
+            Label1.Visible = true;
+            Label1.Text = "Virus Detected and File Can Not Be Uploaded";
+            
+        }
+
+    }
+
+    public bool scanfile()
+    {
+        bool IsValid = true;
+        //foreach (FileInfo file in fileuploadExcel.Unload)
+        //{
+        try
+        {
+            //do av check here
+            Process myProcess = new Process();
+            FileUpload1.SaveAs(Server.MapPath(@"~\upload\") + FileUpload1.FileName);
+            //address of command line virus scan exe
+            myProcess.StartInfo.FileName = @"C:\Program Files (x86)\McAfee\VirusScan Enterprise\scan32.exe";
+            //"C:\\Program Files\\AVG\\AVG2012\\avgscanx.exe";
+
+
+            string path = '"' + "" + Server.MapPath(@"~\upload\") + "" + FileUpload1.FileName + "" + '"';
+            string report = '"' + "" + Server.MapPath(@"~\upload\Report.txt") + "" + '"';
+            string myprocarg = "/SCAN=" + path + " /REPORT=" + report;
+            //" /REPORT=C:\\Upload\\Temp\\Report.txt";
+            myProcess.StartInfo.Arguments = myprocarg;
+            myProcess.StartInfo.CreateNoWindow = true;
+
+            //   myProcess.StartInfo.UseShellExecute = false;
+            // myProcess.StartInfo.RedirectStandardOutput = true;
+
+            myProcess.Start();
+            myProcess.WaitForExit(); //wait for the scan to complete                
+            //add some time for report to be written to file
+            int j = 0;
+            int y = 0;
+            for (j = 0; j <= 1000000; j++)
+            {
+                y = y + 1;
+            }
+            Thread.Sleep(10000);
+
+            //Get a StreamReader class that can be used to read the file
+            StreamReader objStreamReader = default(StreamReader);
+            objStreamReader = File.OpenText(Server.MapPath(@"~\upload\Report.txt"));
+            String reportVerbose = objStreamReader.ReadToEnd().Trim();
+            if (reportVerbose.Length > 0 && !reportVerbose.Contains("Found infections    :    0"))
+            {
+                IsValid = false;
+                File.Delete(Server.MapPath(@"~\upload\") + "" + FileUpload1.FileName);
+            }
+            objStreamReader.Close();
+            if (IsValid)
+            {
+                try
+                {
+                    if (!Directory.Exists(Server.MapPath("~/upload/")))
+                        Directory.CreateDirectory(Server.MapPath("~/upload/"));
+                    if (
+                        !File.Exists(
+                            Server.MapPath(@"~\upload\" +
+                                           System.IO.Path.GetFileName(FileUpload1.PostedFile.FileName))))
+                    {
+                        FileUpload1.SaveAs(Server.MapPath(@"~\DataFiles\") + FileUpload1.FileName);
+                    }
+                    else
+                    {
+                        try
+                        {
+                            File.Delete(
+                                Server.MapPath(@"~\upload\" +
+                                               System.IO.Path.GetFileName(FileUpload1.PostedFile.FileName)));
+                            FileUpload1.SaveAs(Server.MapPath(@"~\DataFiles\") + FileUpload1.FileName);
+                        }
+                        catch (System.IO.IOException)
+                        {
+                        }
+                    }
+                }
+                catch (System.IO.IOException)
+                {
+                }
+
+            }
+            else
+            {
+            }
+
+           
+          
+
+            myProcess.Close();
+        }
+        catch (System.IO.IOException)
+        {
+        }
+
+        return IsValid;
+    }
+  
+
+   
+}
